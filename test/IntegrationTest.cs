e4404093cb33d07883159b19cd4cc2902163916c
Magnus Axelqvist <magnus@axelqvist.com>
2024-03-10 11:23:52 +0100

    Readd test project. Needed to manually delete obj/bin dirs

---
+++ test/IntegrationTest.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class YourTestClass
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // Your test code here
+    }
+}
